8e9d052eb9b35658fbe326d1622194096ed3e290
Aveek Saha <aveek.s98@gmail.com>
2023-05-04 01:44:19 -0400

    Fix update

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -22,17 +22,16 @@ const findTuits = (req, res) => {
 const updateTuit = (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
-    const tuitIndex = tuits.findIndex((t) => t._id === tuitdIdToUpdate);
+    const tuitIndex = tuits.findIndex((t) => t._id == tuitdIdToUpdate);
     tuits[tuitIndex] = { ...tuits[tuitIndex], ...updates };
-    
-    console.log(tuits);
+
     res.sendStatus(200);
 };
 
 const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
     tuits = tuits.filter((t) => t._id !== tuitdIdToDelete);
-    
+
     res.sendStatus(200);
 };
 
