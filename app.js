6e8e915d0cef3594b14ddb934165b960d6441d35
Aveek Saha <aveek.s98@gmail.com>
2023-05-16 19:50:57 -0400

    Await database connection

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-const CONNECTION_STRING =
-    process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
-mongoose.connect(CONNECTION_STRING);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb+srv://aveek:aveekrocks123@cluster0.gbycnql.mongodb.net/?retryWrites=true&w=majority'
+await mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
